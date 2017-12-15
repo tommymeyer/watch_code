5a58fd1e2e5b123a483a38c752ff10dd117a1747
tommymeyer <tlmeyer942@gmail.com>
2017-12-15 14:15:45 -0700

    Initializes and adds first 2 sections of Watch and Code exercises.

---
+++ functions.js
@@ -0,0 +1,41 @@
+// Intro piece of Watch and Code Functions section
+
+// makeTurkeySandwich
+//   Get one slice of bread.
+//   Add turkey.
+//   Put a slice of bread on top.
+
+
+// function makeSandwich(filling) {
+
+// }
+
+// makeSandwich("ham");
+
+
+
+// Todos Function Section
+var todos = ["item 1", "item 2"];
+
+// It should have a function to display todos.
+function displayTodos() {
+  console.log("My todos:", todos);
+}
+
+// It should have a function to add todos.
+function addTodo(todo) {
+  todos.push(todo);
+  displayTodos();
+}
+
+// It should have a function to change todos.
+function changeTodo(position, newValue) {
+  todos[position] = newValue;
+  displayTodos();
+}
+
+// It should have a function to delete todos.
+function deleteTodo(position) {
+  todos.splice(position, 1);
+  displayTodos();
+}
---
+++ requirements.md
@@ -0,0 +1,12 @@
+# V1 Requirements
+* **It should have a** place to store todos
+* **It should have a** way to display todos
+* **It should have a** way to add new todos
+* **It should have a** way to change a todo
+* **It should have a** way to delete a todo
+
+# V2 Requirements
+* **It should have a** function to display todos
+* **It should have a** function to add todos
+* **It should have a** function to change todos
+* **It should have a** function to delete todos
