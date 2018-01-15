28e3010d4ad4b72a9e3dc1dc4ab3b72c9e2037d5
tommymeyer <tlmeyer942@gmail.com>
2018-01-15 14:07:25 -0700

    Adds completed V6.

--- functions.js
+++ functions.js
@@ -1,41 +1,41 @@
-// Intro piece of Watch and Code Functions section
+// // Intro piece of Watch and Code Functions section
 
-// makeTurkeySandwich
-//   Get one slice of bread.
-//   Add turkey.
-//   Put a slice of bread on top.
+// // makeTurkeySandwich
+// //   Get one slice of bread.
+// //   Add turkey.
+// //   Put a slice of bread on top.
 
 
-// function makeSandwich(filling) {
+// // function makeSandwich(filling) {
 
+// // }
+
+// // makeSandwich("ham");
+
+
+
+// // Todos Function Section
+// var todos = ["item 1", "item 2"];
+
+// // It should have a function to display todos.
+// function displayTodos() {
+//   console.log("My todos:", todos);
 // }
 
-// makeSandwich("ham");
+// // It should have a function to add todos.
+// function addTodo(todo) {
+//   todos.push(todo);
+//   displayTodos();
+// }
 
+// // It should have a function to change todos.
+// function changeTodo(position, newValue) {
+//   todos[position] = newValue;
+//   displayTodos();
+// }
 
-
-// Todos Function Section
-var todos = ["item 1", "item 2"];
-
-// It should have a function to display todos.
-function displayTodos() {
-  console.log("My todos:", todos);
-}
-
-// It should have a function to add todos.
-function addTodo(todo) {
-  todos.push(todo);
-  displayTodos();
-}
-
-// It should have a function to change todos.
-function changeTodo(position, newValue) {
-  todos[position] = newValue;
-  displayTodos();
-}
-
-// It should have a function to delete todos.
-function deleteTodo(position) {
-  todos.splice(position, 1);
-  displayTodos();
-}
+// // It should have a function to delete todos.
+// function deleteTodo(position) {
+//   todos.splice(position, 1);
+//   displayTodos();
+// }
