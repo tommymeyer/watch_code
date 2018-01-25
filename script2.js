1385a44bb341b89a4a79c586be596639cae5eb1a
tommymeyer <tlmeyer942@gmail.com>
2018-01-25 15:15:32 -0700

    Adds V6 functionality to script2.

--- script2.js
+++ script2.js
@@ -2,7 +2,19 @@ var todoList = {
   todos: [],
 
   displayTodos: function() {
-    console.log("My todos:", this.todos);
+    if (this.todos.length === 0) {
+      console.log("What's next?");
+    } else {
+      console.log("My todos:");
+
+      for (var i = 0; i < this.todos.length; i++) {
+        if (this.todos[i].completed === false) {
+          console.log("[ ]", this.todos[i].todoText);
+        } else {
+          console.log("[X]", this.todos[i].todoText);
+        }
+      }
+    }
   },
 
   addTodo: function(todoText) {
@@ -28,7 +40,11 @@ var todoList = {
     this.displayTodos();
   },
 
-  deleteTodo: function(position) {
+  toggleAll: function() {
+    
+  },
+
+  deleteTodo: function (position) {
     this.todos.splice(position, 1);
 
     this.displayTodos();
