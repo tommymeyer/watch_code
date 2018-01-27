b91247d3cbcdcce9e8425eb9a3c1cf54a8b39a48
tommymeyer <tlmeyer942@gmail.com>
2018-01-26 17:31:45 -0700

    Adds up to V7 for script 2.

--- script2.js
+++ script2.js
@@ -18,6 +18,8 @@ var todoList = {
   },
 
   addTodo: function(todoText) {
+    debugger;
+    // Every item in the todos array is an object that has text as well as a boolean state for determining completeness.
     this.todos.push({
       todoText: todoText,
       completed: false
@@ -41,7 +43,27 @@ var todoList = {
   },
 
   toggleAll: function() {
-    
+    var completedTodos = 0;
+    var totalTodos = this.todos.length;
+
+    for (var i = 0; i < totalTodos; i++) {
+      if (this.todos[i].completed === true) { // This being a true comparison instead of false is important for the experience. If this were a false comparison, when some of the items were true then toggleAll would make everything false instead of true. We want to toggle all to true if any of the items are true and then, secondarily, if all items are false then toggleAll will make them true.
+        completedTodos++;
+      }
+    }
+    // Case 1: If everything is true, make everything false.
+    if (completedTodos === totalTodos) {
+      for (var i = 0; i < totalTodos; i++) {
+        this.todos[i].completed = false;
+      }
+    // Case 2: Else make everything true.
+    } else {
+      for (var i = 0; i < totalTodos; i++) {
+        this.todos[i].completed = true;
+      }
+    }
+
+    this.displayTodos();
   },
 
   deleteTodo: function (position) {
@@ -50,3 +72,14 @@ var todoList = {
     this.displayTodos();
   }
 };
+
+var displayTodosBtn = document.getElementById("display-todos-btn");
+var toggleAllBtn = document.getElementById("toggle-all-btn");
+
+displayTodosBtn.addEventListener("click", function() {
+  todoList.displayTodos();
+});
+
+toggleAllBtn.addEventListener("click", function() {
+  todoList.toggleAll();
+});
